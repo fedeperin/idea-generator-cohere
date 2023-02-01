a3993fb152df0b9a8b8632a06422803b9b888841
fedeperin <federicoperin@outlook.com.ar>
2023-02-01 14:47:12 -0300

    Change to adaper-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,10 @@
-import adapter from '@sveltejs/adapter-vercel';
- 
-export default {
-  kit: {
-    // default options are shown
-    adapter: adapter({
-      // if true, will deploy the app using edge functions
-      // (https://vercel.com/docs/concepts/functions/edge-functions)
-      // rather than serverless functions
-      edge: false,
- 
-      // an array of dependencies that esbuild should treat
-      // as external when bundling functions. this only applies
-      // to edge functions, and should only be used to exclude
-      // optional dependencies that will not run outside Node
-      external: [],
- 
-      // if true, will split your app into multiple functions
-      // instead of creating a single one for the entire app
-      split: false
-    })
-  }
+import adapter from '@sveltejs/adapter-node';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({ out: 'build' })
+	}
 };
+
+export default config;
