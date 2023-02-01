63cf32d42ac3d01768b620b9a9661b206ae0a6a6
fedeperin <federicoperin@outlook.com.ar>
2023-02-01 14:10:13 -0300

    Revert "Replace cohere-ai package by a fetch"
    This reverts commit eb24b44354889755645213fd91b9ce0326e58917.

--- src/routes/generate/+server.js
+++ src/routes/generate/+server.js
@@ -1,3 +1,4 @@
+import cohere from 'cohere-ai'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -5,39 +6,27 @@ import { prompts } from '$lib/prompts.js'
 
 const { SECRET_COHERE_API_KEY } = process.env
 
-const COHERE_API_GENERATE_URL = 'https://api.cohere.ai/generate'
-
-export const GET = async ({ url, fetch }) => {
+export const GET = async ({ url }) => {
     const randomness = url.searchParams.get('randomness') ?? 1.5
     const promptNumber = url.searchParams.get('prompt') ?? 0
 
-    const data = {
-        model: 'command-xlarge-nightly',
+    cohere.init(SECRET_COHERE_API_KEY)
+
+    const cohereGenerated = await cohere.generate({
+        model: 'command-medium-nightly',
         prompt: prompts[promptNumber],
-        max_tokens: 40,
-        temperature: randomness,
+        max_tokens: 70,
+        temperature: parseFloat(randomness),
         k: 0,
-        p: 1,
+        p: 0.75,
         frequency_penalty: 0,
         presence_penalty: 0,
-        stop_sequences: ['--'],
+        stop_sequences: [],
         return_likelihoods: 'NONE'
-    }
+    })
 
-    const cohereGenerated = await fetch(COHERE_API_GENERATE_URL, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          Authorization: `BEARER ${ SECRET_COHERE_API_KEY }`,
-          "Content-Type": 'application/json',
-          "Cohere-Version": '2022-12-06'
-        },
-        body: JSON.stringify(data)
-    }).then(res => res.json())
-
-      
-    if(cohereGenerated.generations) {
-        const cohereResponseText = cohereGenerated.generations[0].text
+    if(cohereGenerated.body.generations) {
+        const cohereResponseText = cohereGenerated.body.generations[0].text
 
         const formattedCohereResponse = cohereResponseText
         .trim()
